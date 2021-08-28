236426c5a345d706737949f9d5488fa464eb178c
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-08-28 16:55:53 -0700

    Add SingingCosmWasmProvider to _app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from 'components/Layout'
+import { SigningCosmWasmProvider } from 'contexts/cosmwasm'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SigningCosmWasmProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SigningCosmWasmProvider>
+  )
 }
 export default MyApp
