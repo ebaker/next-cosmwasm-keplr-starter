21e17f124e4fbbf3aa08e363b212138067dcef47
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-08-28 17:24:26 -0700

    Add useSigningClient context.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,31 @@
-function Button({ children }) {
-  return (
-    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-      {children}
-    </button>
-  )
-}
+import { useSigningClient } from 'contexts/cosmwasm'
+import Link from 'next/link'
 
 function Nav() {
+  const { walletAddress, connectWallet, disconnect } = useSigningClient()
+  const handleConnect = () => {
+    if (walletAddress.length === 0) {
+      connectWallet()
+    } else {
+      disconnect()
+    }
+  }
+
   return (
     <div className="border-b-2 w-screen px-16">
-      <nav className="flex flex-row w-full justify-between items-center py-2">
-        <div className="font-semibold text-2xl">
-          ⚛️ <span className="pl-2">Next Cosmos Keplr Starter</span>
-        </div>
-        <Button>Connect Wallet</Button>
+      <nav className="flex flex-row w-full justify-between items-center py-4">
+        <Link href="/">
+          <a className="font-semibold text-2xl">
+            ⚛️{' '}
+            <span className="pl-2">{process.env.NEXT_PUBLIC_SITE_TITLE}</span>
+          </a>
+        </Link>
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded truncate"
+          onClick={handleConnect}
+        >
+          {walletAddress || 'Connect Wallet'}
+        </button>
       </nav>
     </div>
   )
