57eb53717f3714d771c8a801ed469509df578612
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-08-29 22:25:00 -0700

    Use daisyui and update alerts. Add responsive styling.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -34,7 +34,7 @@ function Nav() {
           </Link>
         </div>
         <button
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded truncate"
+          className="btn btn-outline btn-primary max-w-full truncate"
           onClick={handleConnect}
         >
           {walletAddress || 'Connect Wallet'}
