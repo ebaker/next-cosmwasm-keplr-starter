eacba1601612497bd43ca6db57fb5ebfa5f059a7
Highlander-Maker <highlander1952@protonmail.com>
2022-06-07 22:30:56 +0100

    build out more page stuff

--- chaintools.tech-ui/pages/index.tsx
+++ chaintools.tech-ui/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
   return (
     <WalletLoader>
       <h1 className="text-6xl font-bold">
-        Welcome to {process.env.NEXT_PUBLIC_CHAIN_NAME} !
+        Select which network to stake to
       </h1>
 
       <div className="mt-3 text-2xl">
@@ -23,9 +23,9 @@ const Home: NextPage = () => {
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
         <Link href="/send" passHref>
           <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-            <h3 className="text-2xl font-bold">Send to wallet &rarr;</h3>
+            <h3 className="text-2xl font-bold">Stake &rarr;</h3>
             <p className="mt-4 text-xl">
-              Execute a trasaction to send funds to a wallet address.
+              Stake you tokens to earn APR yield.
             </p>
           </a>
         </Link>
