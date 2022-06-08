ef5bf2e9ee49c4e8fd96781d5143ac7bb272b075
Highlander-Maker <highlander1952@protonmail.com>
2022-06-08 11:05:14 +0100

    add href to stake button

--- chaintools.tech-ui/pages/index.tsx
+++ chaintools.tech-ui/pages/index.tsx
@@ -1,4 +1,3 @@
-
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import WalletLoader from 'components/WalletLoader'
@@ -9,24 +8,13 @@ const Home: NextPage = () => {
 
   return (
     <WalletLoader>
-      <h1 className="text-6xl font-bold">
-        Select which network to stake to
-      </h1>
-
-      <div className="mt-3 text-2xl">
-        Your wallet address is:{' '}
-        <pre className="font-mono break-all whitespace-pre-wrap">
-          {walletAddress}
-        </pre>
-      </div>
+      <h1 className="text-6xl font-bold">Select network</h1>
 
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
-        <Link href="/send" passHref>
+        <Link href="https://explorer.chaintools.tech/" passHref>
           <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
             <h3 className="text-2xl font-bold">Stake &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Stake you tokens to earn APR yield.
-            </p>
+            <p className="mt-4 text-xl">Stake you tokens to earn APR yield.</p>
           </a>
         </Link>
       </div>
